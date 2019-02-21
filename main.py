d5e09581bb77ff59e47068dcdc0495dcbd8cd1d6
Caterina Clayton <caterina.clayton@gmail.com>
2019-02-21 13:05:04 -0600

    added login, signup templates. Added route handlers in main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash 
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -24,7 +24,7 @@ class Blog(db.Model):
 #Create user class with id, username, password, blogs
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True) 
-    username = db.column(db.String(60), unique=True)
+    username = db.Column(db.String(60), unique=True)
     password = db.Column(db.String(85))
     blogs = db.relationship('blog',backref='owner')
 
@@ -34,7 +34,72 @@ class User(db.Model):
     
     def _repr_(self):
         return str(self.username) 
+# Create login route - validation and verification of user information in database.
+@app.route('/login', methods=['POST','GET'])
+def login():
+    username_error = ""
+    password_error = ""
 
+    if request.method == 'POST':
+        password = request.form['password']
+        username = request.form['username']
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_pw_hash(password, user.pw_hash):
+            session['username'] = username
+            return redirect('/newpost')
+        if not user:
+            return render_template('login.html', username_error="Username does not exist.")
+        else:
+            return render_template('login.html', password_error="Your username or password was incorrect.")
+
+    return render_template('login.html')
+
+# Creat signup route - validation and verification of input.
+@app.route("/signup", methods=['POST', 'GET'])
+def signup():
+        if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+        exist = User.query.filter_by(username=username).first()
+
+        username_error = ""
+        password_error = ""
+        verify_error = ""
+
+        if username == "":
+            username_error = "Please enter a username."
+        elif len(username) <= 3 or len(username) > 20:
+            username_error = "Username must be between 3 and 20 characters long."
+        elif " " in username:
+            username_error = "Username cannot contain any spaces."
+        if password == "":
+            password_error = "Please enter a password."
+        elif len(password) <= 3:
+            password_error = "Password must be greater than 3 characters long."
+        elif " " in password:
+            password_error = "Password cannot contain any spaces."
+        if password != verify or verify == "":
+            verify_error = "Passwords do not match."
+        if exist:
+            username_error = "Username already taken."
+        # If fields are good, continue to creating session with new username and password.
+        if len(username) > 3 and len(password) > 3 and password == verify and not exist:
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            return redirect('/newpost')
+        else:
+            return render_template('signup.html',
+            username=username,
+            username_error=username_error,
+            password_error=password_error,
+            verify_error=verify_error
+            )
+
+    return render_template('signup.html')
 @app.route('/')
 def index():
     blogs = Blog.query.all()
